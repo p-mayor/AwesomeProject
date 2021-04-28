cd17044aee35158aa3e0d05a66f5e3a3b92f2378
petem14 <petem14@gmail.com>
2021-04-28 17:52:15 -0400

    init phot-from-here scaffold

---
+++ services/googleMapsService.js
@@ -0,0 +1,15 @@
+function googleMapsService(state, isReverse) {
+    const apiKey = process.env.REACT_APP_GMAPS_API_KEY
+    const reverseGeoQuery = `address=${state.city}&key=${apiKey}`
+    const geocodeQuery = `latlng=${state.lat},${state.lon}&key=${apiKey}`
+    let URL = `https://maps.googleapis.com/maps/api/geocode/json?`
+    
+    if(isReverse){
+        URL += reverseGeoQuery
+    } else {
+        URL += geocodeQuery
+    }
+    return fetch(URL).then(response => response.json()).catch(err=>console.warn(err))
+}
+
+export default googleMapsService
